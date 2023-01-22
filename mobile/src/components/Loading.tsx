8dc3fb15801872170b5318ddef55d2cbeb943ecb
David Freitas <davidfreitas.dev@outlook.com>
2023-01-22 12:45:30 -0300

    Ajuste git da pasta mobile

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+
+export function Loading() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator color="#7C3AED" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
