0a9db319e2efc7701b3c2ad8cd0b122cbb182fea
David Freitas <davidfreitas.dev@outlook.com>
2023-03-03 15:38:22 -0300

    Deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,8 +8,8 @@ app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-  port: 3333,
-  host: '0.0.0.0'
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333
 })
 .then(() => {
   console.log('HTTP Server Running')
